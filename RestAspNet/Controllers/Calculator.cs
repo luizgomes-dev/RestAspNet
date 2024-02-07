7101bcdafb262f4bf410e6b2404e8a925c26edf0
Distrito <Distrito@DESKTOP-MKFSQC3>
2024-02-07 00:43:47 -0300

    Add project files.

---
+++ RestAspNet/Controllers/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestAspNet.Controllers
+{
+    public class Calculator : Controller
+    {
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            try {
+                var sum = Convert.ToInt32(firstNumber) + Convert.ToInt32(secondNumber);
+
+                return Ok(sum);
+            } catch (FormatException) { 
+                return BadRequest("Digite apenas valores numéricos");
+            }
+        }
+
+        [HttpGet("minus/{firstNumber}/{secondNumber}")]
+        public IActionResult Minus(string firstNumber, string secondNumber)
+        {
+            try
+            {
+                var sum = Convert.ToInt32(firstNumber) - Convert.ToInt32(secondNumber);
+
+                return Ok(sum);
+            }
+            catch (FormatException)
+            {
+                return BadRequest("Digite apenas valores numéricos");
+            }
+        }
+
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        public IActionResult Mult(string firstNumber, string secondNumber)
+        {
+            try
+            {
+                var sum = Convert.ToInt32(firstNumber) * Convert.ToInt32(secondNumber);
+
+                return Ok(sum);
+            } catch (FormatException) {
+                return BadRequest("Digite apenas valores numéricos");
+            }
+        }
+
+        [HttpGet("divide/{firstNumber}/{secondNumber}")]
+        public IActionResult divide(string firstNumber, string secondNumber)
+        {
+            try
+            {
+                var sum = Convert.ToInt32(firstNumber) / Convert.ToInt32(secondNumber);
+
+                return Ok(sum);
+            }
+            catch (FormatException)
+            {
+                return BadRequest("Digite apenas valores numéricos");
+            }
+        }
+    }
+}
